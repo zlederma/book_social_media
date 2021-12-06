9569f814df17cb62af6735422a4e243262680fa7
zlederma <zlederma@ucsd.edu>
2021-12-05 18:01:50 -0800

    started playing around with how many books per row to display on different sized screens

--- src/components/BookCards/BookCards.js
+++ src/components/BookCards/BookCards.js
@@ -1,2 +1,26 @@
 //This component will display all the bookcards
 //6 per row on desktop, 4 per row on tablet, 2 per row on mobile.
+import { Box, Paper, Grid } from '@mui/material'
+import BookCard from "./BookCard"
+
+import React from 'react'
+
+var items = [];
+for (let i = 0; i < 20; i++) {
+    items.push(
+        <Grid item xs={6} sm={3} md={2} lg={2}>
+            <BookCard></BookCard>
+        </Grid>
+    )
+}
+
+
+export default function BookCards() {
+    return (
+        <Box sx={{ flexGrow: 1, margin: 3 }}>
+            <Grid container spacing={2}>
+                {items}
+            </Grid>
+        </Box>
+    )
+}
