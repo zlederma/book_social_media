d0974d19a9513cbce9a3962ca07c64a80be9d7ce
zlederma <zlederma@ucsd.edu>
2021-12-05 18:00:09 -0800

    changed the maxsize of the majority of the content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { background } from "./styles/Styles"
 import './Fonts.css'
-import Book from "./components/BookCards/Book";
+import BookCards from "./components/BookCards/BookCards"
+import NavBar from "./components/NavBar"
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./styles/Theme"
+import { maxWidth } from "@mui/system";
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div style={background}>
-        <Book></Book>
+        <NavBar />
+        {/* maxes the viewport to 1100px */}
+        <div style={{ maxWidth: "1100px", margin: "auto" }}>
+          <BookCards></BookCards>
+        </div>
       </div>
     </ThemeProvider >
   );
