681395329afcf7bd94387e50dada74ad61365df2
zlederma <zlederma@ucsd.edu>
2021-12-08 18:27:05 -0800

    Added a default autocomplete component, and updated the options to be based on the api fetch

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,36 @@
 //this component is used to search for books that can then be added to My Library.
 
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { theme } from "../styles/Theme"
+
+
+
+
 export default function SearchBar({ books }) {
 
     return (
-        <div></div>
+        <div style={{ maxWidth: "400px", margin: "auto" }}>
+            <Autocomplete
+                // <Autocomplete filterOptions={(x) => x} />
+                filterOptions={(x) => x}
+                sx={{ marginTop: 4, backgroundColor: "#f7f3f1" }}
+                freeSolo
+                id="book-search"
+                disableClearable
+                options={books.map((option) => option.title)}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Search for book"
+                        InputProps={{
+                            ...params.InputProps,
+                            type: 'search',
+                        }}
+                    />
+                )}
+            />
+        </div>
     )
 }
+
