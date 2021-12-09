2f4edfaeb10d30a7b3812a4514a61fd772a31cd6
zlederma <zlederma@ucsd.edu>
2021-12-08 22:50:57 -0800

    added inputChangHandler, and added code to give data to App

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,11 +3,24 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { theme } from "../styles/Theme"
+import { useState } from 'react';
 
+export default function SearchBar({ books, onInputValueChange }) {
 
+    const [inputValue, setInputValue] = useState('')
 
+    const inputChangeHandler = (e) => {
+        //if the query is less than 4 charactars an error occurs
+        if (e.target.value.length >= 4) {
+            // console.log(e.target.value);
+            setInputValue(e.target.value);
+            onInputValueChange(inputValue)
+        }
+    };
 
-export default function SearchBar({ books }) {
+    // if (e.key === "Enter") {
+    //     console.log("enter")
+    // }
 
     return (
         <div style={{ maxWidth: "400px", margin: "auto" }}>
@@ -17,6 +30,8 @@ export default function SearchBar({ books }) {
                 sx={{ marginTop: 4, backgroundColor: "#f7f3f1" }}
                 freeSolo
                 id="book-search"
+                onInputChange={inputChangeHandler}
+                onKeyPress={inputChangeHandler}
                 disableClearable
                 options={books.map((option) => option.title)}
                 renderInput={(params) => (
