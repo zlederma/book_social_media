803586ab8b709d2869ae6b4979b636ed1e52ff55
zlederma <zlederma@ucsd.edu>
2021-12-08 16:21:17 -0800

    started work on the fetch request when a user searches for a book.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
+//Styling
 import { background } from "./styles/Styles"
-import './Fonts.css'
+import './GlobalStyles.css'
+import { theme } from "./styles/Theme"
+
+//My Components
 import BookCards from "./components/BookCards/BookCards"
 import NavBar from "./components/NavBar"
-import { ThemeProvider } from "@emotion/react";
-import { theme } from "./styles/Theme"
-import { maxWidth } from "@mui/system";
+import SearchBar from "./components/SearchBar"
 
+//Material UI imports
+import { ThemeProvider } from "@emotion/react";
+
+import { useState } from "react";
+
+//gets API key stored in an environment variable
+const API_KEY = process.env.REACT_APP_API_KEY
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  async function fetchBooksHandler() {
+    setIsLoading(true);
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${API_KEY}`);
+    const data = await response.json();
+    console.log(data);
+
+    const transformedBooks = data.items.map((bookData) => {
+      return {
+        title: bookData.volumeInfo.title,
+        author: bookData.volumeInfo.authors[0],
+        picture: bookData.volumeInfo.imageLinks,
+
+      };
+    });
+    setBooks(transformedBooks);
+    setIsLoading(false);
+  }
+
+  // console.log(books);
+  // console.log(books[0].picture.thumbnail)
+
+
+
   return (
     <ThemeProvider theme={theme}>
       <div style={background}>
         <NavBar />
+        <button onClick={fetchBooksHandler}> click me </button>
+        <SearchBar onChange={fetchBooksHandler} />
         {/* maxes the viewport to 1100px */}
-        <div style={{ maxWidth: "1100px", margin: "auto" }}>
+        <div style={{ maxWidth: "1400px", margin: "auto" }}>
           <BookCards></BookCards>
         </div>
       </div>
