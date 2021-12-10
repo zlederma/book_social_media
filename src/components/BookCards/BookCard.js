4c4e6c06dbb43632c2f64bb583c3f86f628eb398
zlederma <zlederma@ucsd.edu>
2021-12-10 14:15:58 -0800

    Cleaned up the code

--- src/components/BookCards/BookCard.js
+++ src/components/BookCards/BookCard.js
@@ -2,10 +2,6 @@
 // import test_image from "src/assets/test_image.jpg";
 import test_image from "/Users/zoelederman/book_social_media/src/assets/test_image.png"
 
-// import { Image } from "react-bootstrap"
-// import { Image } from "react-bootstrap"
-import { titleStyle } from "./BookCards_Styles"
-
 import * as React from 'react';
 import { Card, CardContent } from '@mui/material';
 import CardActions from '@mui/material/CardActions';
@@ -13,29 +9,14 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { borderBottom } from "@mui/system";
-
-const book = {
-    title: "All the Light we Cannot See",
-    author: "Anthony Doerr",
-    picture: test_image
-}
+import AddBookButton from "./AddBookButton"
 
 
 export default function BookCard({ title, author, picture }) {
-    // if (title === undefined) {
-    //     title = "not known"
-    // }
-
-    // if (picture === undefined) {
-    //     picture = test_image
-    // }
-
-    // if (author === undefined) {
-    //     author = "no author"
-    // }
     return (
         // maxWidth: 200;
         <Card sx={{ backgroundColor: "#f7f3f1" }}>
+            <AddBookButton />
             <CardMedia sx={{ borderBottom: "1px solid #974c0f", objectFit: "scale-down" }}
                 component="img"
                 height="200"
