78f75bd41d29770aa02ccc46e351460a947effa3
zlederma <zlederma@ucsd.edu>
2021-12-20 14:00:41 -0700

    started working on configuring the firebase realtime database

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -7,14 +7,27 @@ import "./SignIn.css"
 import { useState, useContext, useRef } from "react"
 import { flexbox } from '@mui/system'
 import AuthContext from '../../store/auth-context'
+import { getAuth } from "firebase/auth"
+import { database } from '../../firebase/firebase-config'
+import { getDatabase, ref, set } from 'firebase/database';
 
 const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const auth = getAuth()
+
+// function writeUserData(userId, name, email, imageUrl) {
+//     const db = getDatabase();
+//     set(ref(db, 'users/' + userId), {
+//       username: name,
+//       email: email,
+//       profile_picture : imageUrl
+//     });
+//   }
+
 export default function SignIn() {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
 
     const authCtx = useContext(AuthContext);
-
     const [isLogin, setIsLogin] = useState(true);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -52,6 +65,7 @@ export default function SignIn() {
             .then((res) => {
                 setIsLoading(false);
                 if (res.ok) {
+                    console.log(res);
                     return res.json();
                 } else {
                     return res.json().then((data) => {
@@ -71,28 +85,62 @@ export default function SignIn() {
             .catch((err) => {
                 alert(err.message);
             });
+
     };
+
+    const handler = () => {
+        const user = auth.currentUser;
+        console.log(user);
+
+    }
+
 
 
     return (
         <Card elevation={3} style={{ maxWidth: "400px", margin: "auto", backgroundColor: "#f7f3f1", padding: "10px" }}>
-            <Typography variant="h4" sx={{ mb: 1 }} style={{ textAlign: "center" }}> {isLogin ? 'Sign In' : 'Sign Up'} </Typography>
+            <Typography variant="h4"
+                sx={{ mb: 1 }}
+                style={{ textAlign: "center" }}>
+                {isLogin ? 'Sign In' : 'Sign Up'}
+            </Typography>
             <form onSubmit={submitHandler}>
                 <div>
                     <InputLabel htmlFor="email" aria-describedby="my-helper-text"> Email</InputLabel>
-                    <Input className="auth-input" sx={{ mb: 3 }} id="email" inputRef={emailInputRef} fullWidth={true} disableUnderline={true} focused={true} />
+                    <Input
+                        className="auth-input"
+                        sx={{ mb: 3 }}
+                        id="email"
+                        inputRef={emailInputRef}
+                        fullWidth={true}
+                        disableUnderline={true}
+                        focused={true} />
                 </div>
                 <div>
                     <InputLabel htmlFor="password"> Password</InputLabel>
-                    <Input className="auth-input" id="password" inputRef={passwordInputRef} aria-describedby="m-helper-text" fullWidth={true} disableUnderline={true} required />
+                    <Input className="auth-input"
+                        id="password"
+                        inputRef={passwordInputRef} aria-describedby="m-helper-text"
+                        fullWidth={true}
+                        disableUnderline={true}
+                        required />
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Button type='submit' variant="contained" color="secondary" disableRipple sx={{ mt: 3 }}> {isLogin ? 'Submit' : 'Create Account'} </Button>
+                    <Button type='submit'
+                        variant="contained"
+                        color="secondary"
+                        disableRipple sx={{ mt: 3 }}>
+                        {isLogin ? 'Submit' : 'Create Account'}
+                    </Button>
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Button variant="contained" color="secondary" onClick={switchAuthModeHandler} disableRipple sx={{ mt: 3 }}> {isLogin ? 'Create New Account' : 'Login with existing account'} </Button>
+                    <Button variant="text"
+                        color="secondary"
+                        onClick={switchAuthModeHandler}
+                        disableRipple sx={{ mt: 3 }}>
+                        {isLogin ? 'Create New Account' : 'Login with existing account'} </Button>
                 </div>
             </form>
+            <button onClick={handler}> click me </button>
         </Card>
     )
 }
---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
+
+// Set the configuration for your app
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    // measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+export const database = getDatabase(app);
+
