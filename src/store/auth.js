bbbc33d4386d5353ea3f6010151908f79546f5ed
zlederma <zlederma@ucsd.edu>
2021-12-23 12:10:10 -0700

    added a reducer with redux to manage the isLoggedIn state

---
+++ src/store/auth.js
@@ -0,0 +1,27 @@
+//This keeps track of whether or not a user is logged in.
+import { createSlice } from '@reduxjs/toolkit'
+//initally books is an arrary of objects
+const initialIsLoggedInState = { isLoggedIn: false }
+
+
+const isLoggedInSlice = createSlice({
+    name: 'isLoggedIn',
+    initialState: initialIsLoggedInState,
+    reducers: {
+        login(state) {
+            state.isLoggedIn = true;
+        },
+
+        logout(state) {
+            state.isLoggedIn = false;
+        },
+
+        setIsLoggedIn(state, action) {
+            state.isLoggedIn = action.payload;
+        }
+    }
+})
+
+export const isLoggedInActions = isLoggedInSlice.actions;
+
+export default isLoggedInSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 //import my reducers here
 import myBooksReducer from './mybooks'
+import isLoggedInReducer from './auth';
 
 const store = configureStore({
-    reducer: { myBooks: myBooksReducer }
+    reducer: {
+        myBooks: myBooksReducer,
+        isLoggedIn: isLoggedInReducer
+    }
 })
 
 export default store;
